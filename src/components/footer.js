9ea9254fbd82fbc6367fdc61b6e6d263bdf4056f
Gediminas Survila <gediminassurvila@Gediminass-MacBook-Pro.local>
2022-03-01 22:15:10 +0200

    footer text change

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="mt-16 bg-neutral-100 py-8 text-neutral-700">
       <div className="container">
-        <p>Copyright {currentYear}</p>
+        <p>Kopijuoti negalima {currentYear}</p>
       </div>
     </footer>
   )
